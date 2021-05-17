9edddd50daa0a542b83cdaa41ec5b94b67b0c042
sedx876 <swatkins876@gmail.com>
2021-05-17 10:12:07 -0400

    Spinup backend frontend

---
+++ employees/src/main/java/com/sw/employees/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.sw.employees.repository;
+
+import com.sw.employees.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
